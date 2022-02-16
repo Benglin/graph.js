4d1b502a777ffc256bd09b5bdcb9726c04864e56
Benglin <benglin.goh@gmail.com>
2022-02-16 23:32:35 +0800

    Exported all core objects to 'index.js'

--- graph.js/src/index.ts
+++ graph.js/src/index.ts
@@ -1,1 +1,6 @@
 export * from "./core/Graph";
+export * from "./core/GraphEdge";
+export * from "./core/GraphLayer";
+export * from "./core/GraphNode";
+export * from "./core/GraphObject";
+export * from "./core/NodePort";
