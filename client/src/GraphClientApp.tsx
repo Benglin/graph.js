60e47207fd88fd503f45d1a977e30f75b05bce09
Benglin <benglin.goh@gmail.com>
2022-02-16 21:08:34 +0800

    Reset client app contents

---
+++ client/src/GraphClientApp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./GraphClientApp.css";
+
+function GraphClientApp() {
+    return (
+        <div>
+            <div id="graph-container"></div>
+            <div id="react-overlay"></div>
+        </div>
+    );
+}
+
+export default GraphClientApp;
