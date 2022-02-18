a739a1d457dbe34aa6fb917c23a148929913e187
gohb <beng-lin.goh@autodesk.com>
2022-02-18 18:22:12 +0800

    Added 'GraphNodeOptions' and 'NodeType'

--- graph.js/src/core/GraphNode.ts
+++ graph.js/src/core/GraphNode.ts
@@ -3,12 +3,27 @@ import { Size } from "../data/Size";
 import { Vector } from "../data/Vector";
 import { GraphObject } from "./GraphObject";
 
+export enum NodeType {
+    Basic = "Basic",
+}
+
+export interface GraphNodeOptions {
+    nodeType: NodeType | string | undefined;
+}
+
 export class GraphNode extends GraphObject {
+    private readonly _nodeType: string;
     private readonly _rect: Rect;
 
-    constructor() {
+    constructor(options?: GraphNodeOptions) {
         super("node");
+
+        this._nodeType = options?.nodeType ?? NodeType.Basic;
         this._rect = new Rect(new Vector(10, 10), new Size(320, 96));
+    }
+
+    public get nodeType(): string {
+        return this._nodeType;
     }
 
     public get rect(): Rect {
