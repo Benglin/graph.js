6c515c3865d12dfc08da913355aa33b4fe105b41
Benglin <benglin.goh@gmail.com>
2022-02-20 14:39:44 +0800

    Moved various styles to use 'class' attribute

--- graph-js/src/view/SimpleNode.ts
+++ graph-js/src/view/SimpleNode.ts
@@ -1,26 +1,29 @@
-import { EnterElement, Selection } from "d3-selection";
+import { BaseType, EnterElement, Selection } from "d3-selection";
 
 import { GroupSelection } from "../core/Graph";
 import { GraphNode } from "../core/GraphNode";
 import { ViewObject } from "./ViewObject";
 
+import "./styles/SimpleNode.css";
+
 // TODO: Remove this temporary definition.
-interface NodeItems {
+interface NodeItem {
     type: "title" | "sub-title" | "category-heading" | "typed-item";
     primary: string;
     secondary: string;
 }
 
-export type EnterSelection = Selection<EnterElement, NodeItems, HTMLElement, any>;
+export type EnterSelection = Selection<EnterElement, NodeItem, HTMLElement, any>;
+export type NodeElementType = BaseType | SVGRectElement;
 
 export class SimpleNode extends ViewObject {
-    private readonly _items: NodeItems[] = [];
+    private readonly _items: NodeItem[] = [];
     private _nodeGroup: GroupSelection | undefined;
 
     constructor() {
         super("simple-node");
 
-        const items: NodeItems[] = [
+        const items: NodeItem[] = [
             { type: "title", primary: "release", secondary: "2.0.1" },
             { type: "typed-item", primary: "releaseFlcItemUrn", secondary: "String" },
             { type: "category-heading", primary: "release", secondary: "2.0.0" },
@@ -57,40 +60,47 @@ export class SimpleNode extends ViewObject {
             "typed-item": 24.0,
         };
 
-        const colors = {
-            title: "#3c3c3c",
-            "sub-title": "#eeeeee",
-            "category-heading": "#cccccc",
-            "typed-item": "#ffffff",
-        };
-
         const itemHeights = this._items.map((item) => heights[item.type]);
 
-        const itemOffsets: number[] = [0];
-        for (let index = 1; index < itemHeights.length; index++) {
-            const prevHeight = itemHeights[index - 1];
-            const prevOffset = itemOffsets[index - 1];
-            itemOffsets.push(prevOffset + prevHeight + 1);
-        }
+        const itemOffsets: number[] = [];
+        itemHeights.reduce((accumHeight, currHeight, index) => {
+            itemOffsets[index] = accumHeight;
+            return accumHeight + currHeight;
+        }, 0);
 
         this._nodeGroup
             .selectAll("rect")
             .data(this._items)
             .join((e) => e.append("rect"))
-            .attr("width", 200)
             .attr("height", (d, i) => itemHeights[i])
-            .attr("x", 10)
-            .attr("y", (d, i) => `${10 + itemOffsets[i]}`)
-            .style("fill", (d) => colors[d.type]);
+            .attr("y", (d, i) => itemOffsets[i])
+            .each(SimpleNode._augmentElement);
     }
 
     private _ensureGroupCreated(layerGroup: GroupSelection): void {
         if (!this._nodeGroup) {
-            this._nodeGroup = layerGroup.append("g").attr("id", `${this.id}`);
+            this._nodeGroup = layerGroup
+                .append("g")
+                .attr("id", `${this.id}`)
+                .attr("transform", "translate(10, 10)")
+                .classed("simple-node", true);
         }
     }
 
-    private static _enter(enter: EnterSelection) {
-        return enter.append("rect");
+    // 'this' refers to the Element that this function is called for.
+    private static _augmentElement(
+        this: NodeElementType,
+        datum: NodeItem,
+        index: number,
+        groups: NodeElementType[] | ArrayLike<NodeElementType>
+    ): void {
+        const element = this as Element;
+        element.classList.add(datum.type);
+
+        // Position the current element based on height of the previous one.
+        if (index === 2) {
+            const e = groups[index - 1] as Element;
+            console.log(`Previous height: ${e.clientHeight}`);
+        }
     }
 }
