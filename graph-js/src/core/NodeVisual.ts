e3778f919a0a639d4ca56ff49f233eec9ae66eeb
gohb <beng-lin.goh@autodesk.com>
2022-02-21 16:01:09 +0800

    Defined 'INodeVisual' interface/class

---
+++ graph-js/src/core/NodeVisual.ts
@@ -0,0 +1,25 @@
+import { IVisualContext } from "./VisualContext";
+import { GroupSelection } from "./TypeDefinitions";
+
+export interface INodeVisual {
+    render(context: IVisualContext, layerGroup: GroupSelection): void;
+}
+
+export abstract class NodeVisual implements INodeVisual {
+    private readonly _id: string;
+
+    constructor(idPrefix: string) {
+        const id = Math.random().toString().substring(2);
+        this._id = `${idPrefix}-id`;
+    }
+
+    public get id(): string {
+        return this._id;
+    }
+
+    public abstract render(context: IVisualContext, layerGroup: GroupSelection): void;
+}
+
+export interface ViewObjectIdMap {
+    [nodeId: string]: NodeVisual;
+}
