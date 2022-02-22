5728e2224a5624aecf6fbebac5126a982e0a2a1f
gohb <beng-lin.goh@autodesk.com>
2022-02-22 18:21:27 +0800

    Updated 'NodePort' definition

--- graph-js/src/core/NodePort.ts
+++ graph-js/src/core/NodePort.ts
@@ -1,12 +1,20 @@
-export enum PortType {
-    Input = "Input",
-    Output = "Output",
+import { Vector } from "../data/Vector";
+
+enum PortAttachment {
+    None = 0,
+    North = 1,
+    East = 2,
+    South = 3,
+    West = 4,
+    Free = 5,
 }
 
-export interface NodePort {
-    nodeId: string;
-    portId: string;
-    portType: PortType;
+interface NodePort {
+    id: string;
+    attachment: PortAttachment;
+    offset: number | string;
+    position: Vector;
+    normal: Vector;
 }
 
 export interface NodePorts {
