1226ddc96900858ad59051e95bc91a32f4bd60e0
gohb <beng-lin.goh@autodesk.com>
2022-02-28 10:31:45 +0800

    Added 'IGraphObjectFactory.createGraphObject'

--- client/src/graph/views/GraphObjectFactory.ts
+++ client/src/graph/views/GraphObjectFactory.ts
@@ -1,8 +1,20 @@
-import { IGraphObjectFactory, IGraphObjectVisual } from "graph-js";
+import { GraphObject, IGraphObjectFactory, IGraphObjectVisual } from "graph-js";
 import { SchemaNodeVisual } from "./SchemaNodeVisual";
 import { SchemaEdgeVisual } from "./SchemaEdgeVisual";
+import { SchemaNode } from "../nodes/SchemaNode";
 
 export class GraphObjectFactory implements IGraphObjectFactory {
+    createGraphObject(objectType: string, data: any): GraphObject {
+        switch (objectType) {
+            case "simple-node":
+                return SchemaNode.fromData(data);
+            case "simple-edge":
+                break;
+        }
+
+        throw new Error("Handle this");
+    }
+
     createObjectVisual(objectType: string): IGraphObjectVisual {
         switch (objectType) {
             case "simple-node":
--- graph-js/src/core/GraphObjectFactory.ts
+++ graph-js/src/core/GraphObjectFactory.ts
@@ -1,5 +1,7 @@
+import { GraphObject } from "./GraphObject";
 import { IGraphObjectVisual } from "./GraphObjectVisual";
 
 export interface IGraphObjectFactory {
+    createGraphObject(objectType: string, data: any): GraphObject;
     createObjectVisual(objectType: string): IGraphObjectVisual;
 }
