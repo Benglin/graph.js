edb2be161d0a48e9d851e547f496203891db661b
gohb <beng-lin.goh@autodesk.com>
2022-03-11 18:33:38 +0800

    Added 'Layout' button to the main screen

--- client/src/GraphClientApp.tsx
+++ client/src/GraphClientApp.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import Button from "@mui/material/Button";
 import "./GraphClientApp.css";
 import { ReactGraph } from "./components/ReactGraph";
 
 function GraphClientApp() {
     return (
-        <div>
-            <div className="overlay-back">
+        <div className="outer-container">
+            <div className="button-host">
+                <Button>Layout</Button>
+            </div>
+            <div className="content-host">
                 <ReactGraph />
             </div>
-            <div className="overlay-front"></div>
         </div>
     );
 }
