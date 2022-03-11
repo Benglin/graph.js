0369832b96dfe64a9c022aea5bc1205216a9f646
gohb <beng-lin.goh@autodesk.com>
2022-03-12 00:11:23 +0800

    Added button to remove two nodes

--- client/src/GraphClientApp.tsx
+++ client/src/GraphClientApp.tsx
@@ -21,11 +21,35 @@ function GraphClientApp() {
         }
     }
 
+    function handleRemoveClick(): void {
+        if (graphRef.current) {
+            graphRef.current.removeNodes([
+                "simple-node-d9881c04-2fd6-4f6c-9d30-be3b434d3619",
+                "simple-node-7ea797d7-90ad-4fbb-8b94-a74f61e29589",
+            ]);
+        }
+    }
+
     return (
         <div className="outer-container">
             <div className="button-host">
-                <Button onClick={handleLayoutClick} disabled={layoutInProgress} variant="contained">
+                <Button
+                    sx={{ mb: 1 }}
+                    onClick={handleLayoutClick}
+                    disabled={layoutInProgress}
+                    variant="contained"
+                    fullWidth
+                >
                     Layout
+                </Button>
+                <Button
+                    sx={{ mb: 1 }}
+                    onClick={handleRemoveClick}
+                    disabled={layoutInProgress}
+                    variant="contained"
+                    fullWidth
+                >
+                    Remove
                 </Button>
             </div>
             <div className="content-host">
